17b3d7d4b1654f01ed8d1f20099c6578d907a2ea
GiuseppeMandriani <giuseppe.mandriani@gmail.com>
2021-05-04 23:55:50 +0200

    add header

---
+++ src/components/Header.vue
@@ -0,0 +1,124 @@
+<template>
+<header>
+    <div class="container">
+        <!-- Logo -->
+        <img class="logo" src="../assets/img/dc-logo.png" alt="Logo DC">
+
+        <!-- Nav Bar -->
+        <nav>
+            <ul class="nav-menu">
+                <li v-for="(link,index) in links" :key="index">
+                    <a href="">{{ link.text }}</a>
+                </li>
+            </ul>
+        </nav>
+
+    </div>
+</header>
+  
+</template>
+
+<script>
+export default {
+    // Componente
+    name: 'Header',
+
+    // Data come funzione
+        data() {
+       return {
+           links: [
+               { 
+                    text: "characters",
+                    url: "/characters",
+                    current: false
+                },
+               { 
+                   text: "comics", 
+                   url: "/comics", 
+                   current: true
+                },
+               { 
+                   text: "movies", 
+                   url: "/movies", 
+                   current: false
+                },
+               { 
+                   text: "tv", 
+                   url: "/tv", 
+                   current: false
+                },
+               { 
+                   text: "games", 
+                   url: "/games", 
+                   current: false
+                },
+               { 
+                   text: "collectilbles", 
+                   url: "/collectibles", 
+                   current: false
+                },
+               { 
+                   text: "videos", 
+                   url: "/videos", 
+                   current: false
+                },
+               { 
+                   text: "fans", 
+                   url: "/fans", 
+                   current: false
+                },
+               { 
+                   text: "news", 
+                   url: "/news", 
+                   current: false
+               },
+               { 
+                   text: "shop", 
+                   url: "/shop", 
+                   current: false
+                },
+           ],
+       }
+    }
+}
+
+
+</script>
+
+<style scoped>
+.container{
+    padding: 30px 0;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    height: 100px;
+}
+
+/* Logo */
+.logo{
+    max-width: 80px;
+}
+
+.nav-menu{
+    display: flex;
+    list-style: none;
+
+}
+
+
+.nav-menu li a{
+    margin-right: 1rem;
+    text-transform: uppercase;
+    text-decoration: none;
+    color: #1c1c1c;
+    font-size:12px;
+    border-bottom: 2px solid transparent;
+    transition: .3s;
+}
+
+.nav-menu li a:hover,
+.nav-menu li a:active{
+    border-bottom: 2px solid dodgerblue;
+
+}
+</style>
