ac3bdd1ae1a6dc9ab13c130946407ee3c4253178
GiuseppeMandriani <giuseppe.mandriani@gmail.com>
2021-05-05 22:37:05 +0200

    ok

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -19,6 +19,8 @@
 </template>
 
 <script>
+import seriesData from '@/data/seriesData.js'
+
 export default {
     // Componente
     name: 'Header',
@@ -27,58 +29,7 @@ export default {
         data() {
        return {
            indexLink: 0,
-           links: [
-               { 
-                    text: "characters",
-                    url: "/characters",
-                    current: false
-                },
-               { 
-                   text: "comics", 
-                   url: "/comics", 
-                   current: true
-                },
-               { 
-                   text: "movies", 
-                   url: "/movies", 
-                   current: false
-                },
-               { 
-                   text: "tv", 
-                   url: "/tv", 
-                   current: false
-                },
-               { 
-                   text: "games", 
-                   url: "/games", 
-                   current: false
-                },
-               { 
-                   text: "collectilbles", 
-                   url: "/collectibles", 
-                   current: false
-                },
-               { 
-                   text: "videos", 
-                   url: "/videos", 
-                   current: false
-                },
-               { 
-                   text: "fans", 
-                   url: "/fans", 
-                   current: false
-                },
-               { 
-                   text: "news", 
-                   url: "/news", 
-                   current: false
-               },
-               { 
-                   text: "shop", 
-                   url: "/shop", 
-                   current: false
-                },
-           ],
+           links: seriesData,
        }
     }
 }
